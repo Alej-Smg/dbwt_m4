dc54842697d8bc66e8260dcd9b6708cbb6dc5025
Alej-Smg <alexschmeing@web.de>
2021-12-01 16:58:17 +0100

    Alles bis Aufgabe 5 fertig

--- werbeseite/wunschgericht.php
+++ werbeseite/wunschgericht.php
@@ -56,17 +56,31 @@ $name_ersteller = "Anonym";
 $email_ersteller;
 
 if (isset($_POST["name_Wgericht"]) && !$link->connect_error){
+	$countAnon;
+	if (file_exists("countAnon.json")) {
+		$countAnon = json_decode(file_get_contents("countAnon.json"), true);
+	} else $countAnon = 0;
+
+	$countAnon ++;
 	// Variablen übertragen
 	if (isset($_POST["name_Wgericht"])) $name_Wgericht = $_POST["name_Wgericht"];
 	if (isset($_POST["comment_Wgericht"])) $comment_Wgericht = $_POST["comment_Wgericht"];
 	if (isset($_POST["name_ersteller"])) $name_ersteller = $_POST["name_ersteller"];
+	if (strlen($name_ersteller) < 1) $name_ersteller = "Anonym" . $countAnon;
 	if (isset($_POST["email_ersteller"])) $email_ersteller = $_POST["email_ersteller"];
+
+	file_put_contents("countAnon.json", json_encode($countAnon));
+	unset($countAnon);
 
 	
 	// Querys erstellen
-	$SQL_insert_ersteller = "INSERT INTO ersteller (Name, E_Mail) VALUES ('$name_ersteller', '$email_ersteller');";
-	$SQL_insert_wunschgericht = "INSERT INTO wunschgericht (Name, Beschreibung, Erstellt_von) 
-		VALUES ('$name_Wgericht', '$comment_Wgericht', '$name_ersteller');";
+	$stmt = $link->prepare("INSERT INTO ersteller (Name, E_Mail) VALUES (?, ?)");
+    $stmt->bind_param("ss", $name_ersteller, $email_ersteller);
+    $stmt->execute();
+
+	$stmt = $link->prepare("INSERT INTO wunschgericht (Name, Beschreibung, Erstellt_von) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $name_Wgericht, $comment_Wgericht, $name_ersteller);
+    $stmt->execute();
 
 /**
  * $stmt = $link->prepare("INSERT INTO wunschgericht (vorname, email, gerichtname, beschreibung) VALUES (?, ?, ?, ?)");
@@ -74,10 +88,10 @@ if (isset($_POST["name_Wgericht"]) && !$link->connect_error){
  *  $stmt->execute();
  */
 	// Querys an server schicken
-	if ($link->query($SQL_insert_ersteller)) echo "query 1 true\n";
-	else echo $link -> error . "\n";
-	if ($link->query($SQL_insert_wunschgericht)) echo "query 2 true\n";
-	else echo "query 2 fail\n";
+	// if ($link->query($SQL_insert_ersteller)) echo "query 1 true\n";
+	// else echo $link -> error . "\n";
+	// if ($link->query($SQL_insert_wunschgericht)) echo "query 2 true\n";
+	// else echo "query 2 fail\n";
 
 	echo "Vielen Dank für Ihre Einsendung\n";
 
